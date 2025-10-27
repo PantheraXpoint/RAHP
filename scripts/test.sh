b5a01a92158680b6a7960319a61a54a9783eed32
PantheraXpoint <nhatquang.panthera0207@gmail.com>
2025-10-27 22:40:55 +0900

    update testing script

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,18 +7,28 @@ echo "Testing!!!!"
 # MODEL_NAME="11-6-SGDet-close-set-RAHP-without-distillation-loss"
 MODEL_NAME="glip_zeroshot_test"
 
-python tools/test_grounding_net.py \
+# python tools/test_grounding_net.py \
+#     --task_config configs/vg150/finetune.yaml \
+#     --config-file configs/pretrain/glip_Swin_T_O365_GoldG.yaml \
+#     SOLVER.IMS_PER_BATCH $(( 1 * ${NUM_GPUS} )) \
+#     TEST.IMS_PER_BATCH $(( 1 * ${NUM_GPUS} )) \
+#     MODEL.DYHEAD.RELATION_REP_REFINER False \
+#     MODEL.ROI_RELATION_HEAD.PREDICT_USE_BIAS False \
+#     MODEL.ROI_RELATION_HEAD.USE_GT_BOX True \
+#     MODEL.ROI_RELATION_HEAD.USE_GT_OBJECT_LABEL True \
+#     DATASETS.VG150_OPEN_VOCAB_MODE False \
+#     MODEL.WEIGHT MODEL/glip_tiny_model_o365_goldg_cc_sbu.pth \
+#     MODEL.LANGUAGE_BACKBONE.MODEL_TYPE bert-base-uncased \
+#     MODEL.LANGUAGE_BACKBONE.MODEL_DIR MODEL/bert-base-uncased \
+#     MODEL.LANGUAGE_BACKBONE.TOKENIZER_LOCAL_FILES_ONLY True \
+#     OUTPUT_DIR ./OUTPUT/glip_zeroshot_test
+
+
+CUDA_VISIBLE_DEVICES=3 python tools/test_grounding_net.py \
     --task_config configs/vg150/finetune.yaml \
     --config-file configs/pretrain/glip_Swin_T_O365_GoldG.yaml \
-    SOLVER.IMS_PER_BATCH $(( 1 * ${NUM_GPUS} )) \
-    TEST.IMS_PER_BATCH $(( 1 * ${NUM_GPUS} )) \
-    MODEL.DYHEAD.RELATION_REP_REFINER False \
-    MODEL.ROI_RELATION_HEAD.PREDICT_USE_BIAS False \
-    MODEL.ROI_RELATION_HEAD.USE_GT_BOX True \
-    MODEL.ROI_RELATION_HEAD.USE_GT_OBJECT_LABEL True \
-    DATASETS.VG150_OPEN_VOCAB_MODE False \
     MODEL.WEIGHT MODEL/glip_tiny_model_o365_goldg_cc_sbu.pth \
-    MODEL.LANGUAGE_BACKBONE.MODEL_TYPE bert-base-uncased \
     MODEL.LANGUAGE_BACKBONE.MODEL_DIR MODEL/bert-base-uncased \
     MODEL.LANGUAGE_BACKBONE.TOKENIZER_LOCAL_FILES_ONLY True \
-    OUTPUT_DIR ./OUTPUT/glip_zeroshot_test
+    MODEL.DYHEAD.OV.DYNAMIC_CLIP_CLASSIFIER_WEIGHT_CACHE_PTH "MODEL/relation_aware_weight_cache.pth" \
+    OUTPUT_DIR ./OUTPUT/rahp_paper_30entities_test
